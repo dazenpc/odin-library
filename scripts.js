bb6dcc49704e72e8a89f8e27ca43de9d22d52565
dazenpc <121750352+dazenpc@users.noreply.github.com>
2025-04-10 17:23:02 +0530

    feat: add styles & display book logic

--- scripts.js
+++ scripts.js
@@ -23,3 +23,97 @@ function addBookToLibrary(title, author, noOfPages, haveRead) {
   
 }
 
+addBookToLibrary("delete", "Its a me", 30, false);
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false);
+
+addBookToLibrary("Book 1", "Its a me", 30, false);
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false);
+addBookToLibrary("Book 1", "Its a me", 30, false);
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false);
+addBookToLibrary("Book 1", "Its a me", 30, false);
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false);
+addBookToLibrary("Book 1", "Its a me", 30, false);
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false); 
+
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false);
+addBookToLibrary("Book 1", "Its a me", 30, false);
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false);
+addBookToLibrary("Book 1", "Its a me", 30, false);
+addBookToLibrary("Book 2", "Its a me", 30, true);
+addBookToLibrary("Book 3", "Its a me", 30, false);  
+
+
+function displayBook(){
+  // get the main div
+  const displayArea = document.querySelector(".main");
+
+  // loop over the library object
+  for(let book of myLibrary){
+    const card = document.createElement('div');
+    card.setAttribute("style", 
+      `border: 1px solid black; border-radius: 10px; padding: 10px; width: 20rem; white-space: wrap; overflow: auto;`)
+    
+    // get the book
+    let actualBook = book.book;
+
+    // get details and add to card
+    const title = document.createElement('p');
+    title.innerText = `Title : ${actualBook.title}`;
+    card.appendChild(title);
+
+    const author = document.createElement('p');
+    author.innerText = `Author : ${actualBook.author}`;
+    card.appendChild(author);
+
+    const pages = document.createElement('p');
+    pages.innerText = `Pages : ${actualBook.noOfPages}`;
+    card.appendChild(pages);
+
+    const haveRead = document.createElement('p');
+    haveRead.innerText = `Completed : ${actualBook.haveRead? 'Yes':'No'}`;
+    card.appendChild(haveRead);
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "delete"
+    card.appendChild(removeButton);
+
+
+    function completeButtonCreator(){
+    const completeButton = document.createElement('button');
+    completeButton.innerText = "complete";
+    card.appendChild(completeButton);
+    completeButton.addEventListener("click", ()=>{
+      actualBook.haveRead = true;
+      changeCardColor();
+      card.removeChild(completeButton);
+    })
+    }
+    if(!actualBook.haveRead) completeButtonCreator();
+
+    // color based on completion
+    function changeCardColor(){
+      let color = actualBook.haveRead? "#90EE90":"#FF7F7F"
+      card.style.backgroundColor = color;
+    }
+    changeCardColor();
+    
+    // add card to display
+    displayArea.appendChild(card);
+
+    removeButton.addEventListener("click", ()=>{
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      displayArea.removeChild(card);
+    });
+
+  }
+
+}
+
+displayBook();
